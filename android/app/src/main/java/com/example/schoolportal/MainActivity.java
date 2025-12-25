4bd654c5884bedc58b35b4ee5905bbab8ba6a6d2
Lakshaaaa1 <lllaaaallaaaaa@gmail.com>
2025-12-25 22:10:50 +0530

    delete all files from repo

--- android/app/src/main/java/com/example/schoolportal/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.schoolportal;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-    // Capacitor automatically registers plugins via the Capacitor gradle plugin
-}
--- attached_assets/20250709151815-41920561-ae5a-4aff-bd03-5dd2a46c7de4_1754308270286.sql
+++
@@ -1,7 +0,0 @@
-
--- Remove all attendance-related data and tables
-DROP TABLE IF EXISTS public.attendance_links CASCADE;
-
--- Remove any attendance-related triggers or functions if they exist
-DROP FUNCTION IF EXISTS public.handle_attendance_update() CASCADE;
-DROP FUNCTION IF EXISTS public.calculate_attendance_percentage() CASCADE;
